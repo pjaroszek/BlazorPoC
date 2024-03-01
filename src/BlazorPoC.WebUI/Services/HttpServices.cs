3921c7526ab5da825c2f664ae5cdd3eb664c2dd9
Pawel Jaroszek <p.jaroszek@gmail.com>
2024-03-01 16:22:39 +0100

    Added HttpClienta and get waetherForecast data from Api

---
+++ src/BlazorPoC.WebUI/Services/HttpServices.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Json;
+using BlazorPoC.WebUI.Models;
+using BlazorPoC.WebUI.Pages;
+
+namespace BlazorPoC.WebUI.Services;
+
+
+internal sealed class HttpServices
+{
+    private readonly HttpClient httpClient;
+
+    public HttpServices(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+    
+    public async Task<IEnumerable<WeatherForecast>> GetWeatherForecastData()
+    {
+        var results = await this.httpClient.GetAsync("/weatherforecast");
+
+        Console.WriteLine(results.Content);
+        return await results.Content.ReadFromJsonAsync<IEnumerable<WeatherForecast>>()?? new List<WeatherForecast>();
+    }
+}
